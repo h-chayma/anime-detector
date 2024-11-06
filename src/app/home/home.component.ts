7f0e62e3edf3e6163b62d1ffba5f0deb1d19f73e
h-chayma <chaymafarhat16@gmail.com>
2024-11-06 15:04:19 +0100

    Other improvements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,27 +21,55 @@ export class HomeComponent {
 
     if (fileList && fileList.length > 0) {
       const file: File = fileList[0];
-      const formData = new FormData();
-      formData.append('image', file, file.name);
-      const imageUrl = URL.createObjectURL(file);
-      this.loading = true;
-
-      this.searchService.searchImageAndResize(file).subscribe((result) => {
-        this.searchService.setSearchResult(result);
-        this.router.navigate(['/result', imageUrl]);
-      }, (error) => {
-        console.error('Error during search:', error);
-      },
-        () => {
-          this.loading = false;
-        });
-
-      const fileForm: HTMLFormElement | null = document.querySelector('#fileForm');
-      if (fileForm) {
-        fileForm.reset();
-      }
+      this.handleFile(file);
     } else {
       this.errorMessage = 'No file selected. Please choose an image file to upload.';
+    }
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault(); // Prevent default to allow drop
+    event.dataTransfer!.dropEffect = 'copy'; // Indicate copy action
+    const dropZone = event.currentTarget as HTMLElement;
+    dropZone.classList.add('dragging'); // Add class to style drop zone
+  }
+
+  onDragLeave(event: DragEvent) {
+    const dropZone = event.currentTarget as HTMLElement;
+    dropZone.classList.remove('dragging'); // Remove class when drag leaves
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault(); // Prevent default behavior
+    const dropZone = event.currentTarget as HTMLElement;
+    dropZone.classList.remove('dragging'); // Remove dragging class
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      const file: File = files[0];
+      this.handleFile(file);
+    }
+  }
+
+  private handleFile(file: File) {
+    const formData = new FormData();
+    formData.append('image', file, file.name);
+    const imageUrl = URL.createObjectURL(file);
+    this.loading = true;
+
+    this.searchService.searchImageAndResize(file).subscribe((result) => {
+      this.searchService.setSearchResult(result);
+      this.router.navigate(['/result', imageUrl]);
+    }, (error) => {
+      console.error('Error during search:', error);
+    },
+      () => {
+        this.loading = false;
+      });
+
+    const fileForm: HTMLFormElement | null = document.querySelector('#fileForm');
+    if (fileForm) {
+      fileForm.reset();
     }
   }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  logoSrc: string = 'assets/img/logo.png'; // Default logo for light mode
 
+  constructor() {
+    this.updateLogo();
+  }
+  ngOnInit() {
+    this.updateLogo(); // Initial check on component load
+  }
+
+  updateLogo() {
+    const theme = document.documentElement.getAttribute('data-theme-version');
+    if (theme === 'dark') {
+      this.logoSrc = 'assets/img/logo-dark.png'; // Logo for dark mode
+    } else {
+      this.logoSrc = 'assets/img/logo.png'; // Default logo for light mode
+    }
+  }
 }
